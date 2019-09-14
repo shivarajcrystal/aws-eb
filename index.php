5df8455518e76e750c8eb28059101ad4410240e1
shivarajcrystal <55010973+shivarajcrystal@users.noreply.github.com>
2019-09-14 12:06:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-echo "uttam ganesh archana vinod pratik vishwas shivaraj vishwa  this is the aws devops automation class added into the php file";
+echo "uttam ganesh archana vinod pratik vishwas shivaraj vishwa  this is the aws devops automation class added into the php file shivraj";
 ?>
 
 </body>
